36caf75ce7e094b382251e4e443412d47135fae2
lsj1759 <lsj1759@naver.com>
2021-09-17 10:41:25 +0900

    수정

--- project1/source.cpp
+++ project1/source.cpp
@@ -7,10 +7,12 @@ int main()
 	int HP = 0;
 	float MP = 10.2f;
 	char SP = 'H';
+	char TP = 'Y';
 
 	cout << HP << endl;
 	cout << MP << endl;
 	cout << SP << endl;
+	cout << TP << endl;
 
 	return 0;
 }
