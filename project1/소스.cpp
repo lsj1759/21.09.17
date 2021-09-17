d60107c72a13d4caa7000cfaaf947e95754c8b43
lsj1759 <lsj1759@naver.com>
2021-09-17 09:22:50 +0900

    int, float, char 출력하기

---
+++ project1/소스.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int HP = 0;
+	float MP = 10.2f;
+	char SP = 'H';
+
+	cout << HP << endl;
+	cout << MP << endl;
+	cout << SP << endl;
+
+	return 0;
+}
