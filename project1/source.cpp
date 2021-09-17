8feca3847a0a441d8cc5e268d60650a5fa1dae8c
lsj1759 <lsj1759@naver.com>
2021-09-17 13:28:31 +0900

    합 구하기

--- project1/source.cpp
+++ project1/source.cpp
@@ -5,14 +5,17 @@ using namespace std;
 int main()
 {
 	int HP = 0;
-	float MP = 10.2f;
-	char SP = 'H';
-	char TP = 'Y';
+	int MP = 0;
+	//float MP = 10.2f;
+	//char SP = 'H';
+	//char TP = 'Y';
 
-	cout << HP << endl;
-	cout << MP << endl;
-	cout << SP << endl;
-	cout << TP << endl;
+	// 키보드 입력 받아서 HP와 MP에 저장
+	cin >> HP;
+	cin >> MP;
+
+	cout << HP + MP << endl;
+
 
 	return 0;
 }
